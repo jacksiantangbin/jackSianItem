017d3ff17f1156350c5150e19489bde3ed92e439
jacksian <15929379760@163.com>
2019-12-13 15:54:53 +0800

    二次更新超级员工

---
+++ super_staff_two/pages/actionPage/index.js
@@ -0,0 +1,161 @@
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+var user = require('../../services/user.js');
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+    // gr_small: 'http://image-test.wemeke.com/tenant/small.png',
+    // gr_big: 'http://image-test.wemeke.com/tenant/big.png',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    wx.showLoading({
+      title: '加载中...',
+    })
+    user.checkLogin().then(res => {
+      console.log('缓存')
+      if (wx.getStorageSync('userInfo') != null) {
+        this.globalData.userInfo = wx.getStorageSync('userInfo');
+        this.globalData.token = wx.getStorageSync('token');
+        this.globalData.userId = wx.getStorageSync('userId');
+        this.globalData.sessionKey = wx.getStorageSync('userInfo.sessionKey');
+        console.log(this.globalData.sessionKey + '我是1')
+      } else {
+        user.loginByWeixin().then(res => {
+          this.globalData.userInfo = res.data.userInfo;
+          this.globalData.token = res.data.token;
+          this.globalData.userId = res.data.userId;
+          this.globalData.sessionKey = res.data.userInfo.sessionKey;
+          wx.setStorageSync('userInfo', res.data.userInfo);
+          wx.setStorageSync('token', res.data.token);
+          wx.setStorageSync('userId', res.data.userId);
+          wx.setStorageSync('sessionKey', res.data.userInfo.sessionKey);
+          console.log(this.globalData.sessionKey + '我是2')
+        }).catch((err) => {
+          console.log(err)
+        });
+      }
+    }).catch(() => {
+      console.log('再次登录')
+      //再次登录..
+      user.loginByWeixin().then(res => {
+        this.globalData.userInfo = res.data.userInfo;
+        this.globalData.token = res.data.token;
+        this.globalData.userId = res.data.userId;
+        this.globalData.sessionKey = res.data.userInfo.sessionKey;
+        wx.setStorageSync('userInfo', res.data.userInfo);
+        wx.setStorageSync('token', res.data.token);
+        wx.setStorageSync('userId', res.data.userId);
+        wx.setStorageSync('sessionKey', res.data.userInfo.sessionKey);
+        console.log(this.globalData.sessionKey + '我是2')
+      }).catch((err) => {
+        console.log(err)
+      });
+    });
+  },
+  gr_img: function() {
+    var that = this
+    var data = {}
+    data.tenantId = app.globalData.tenantId
+    util.request(api.actionPage,data).then(function(res) {
+      if (res.errno == 0) {
+        if (res.data.data.logo) {
+          that.setData({
+            gr_small: res.data.data.logo,
+          })
+        } else {
+          that.setData({
+            gr_small: 'http://image-test.wemeke.com/tenant/small.png',
+          })
+        }
+      } else {
+        that.setData({
+          gr_small: 'http://image-test.wemeke.com/tenant/small.png',
+        })
+      }
+    });
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+    var that = this
+    setTimeout(function () {
+      wx.hideLoading()
+      that.gr_img()
+    }, 2000)
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  // onShareAppMessage: function() {
+
+  // }
+})
+
+
+// "tabBar": {
+//   "backgroundColor": "#ffffff",
+//   "borderStyle": "black",
+//   "selectedColor": "#4298dc",
+//   "color": "#888888",
+//   "list": [{
+//       "pagePath": "pages/index/index",
+//       "iconPath": "static/images/home.png",
+//       "selectedIconPath": "static/images/home_selected.png",
+//       "text": "首页"
+//     },
+//     {
+//       "pagePath": "pages/my/index/index",
+//       "iconPath": "static/images/my.png",
+//       "selectedIconPath": "static/images/my_selected.png",
+//       "text": "我的"
+//     }
+//   ]
+// },
---
+++ super_staff_two/pages/my/index/index.js
@@ -0,0 +1,292 @@
+const util = require('../../../utils/util.js');
+const api = require('../../../config/api.js');
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    myPhone: '/img/my.png',
+    install: '/img/install.png',
+    collect: '/img/collect.png',
+    evaluate: '/img/evaluate.png',
+    coupon: '/img/coupon.png',
+    good: '/img/good.png',
+    order: '/img/order.png',
+    dialog: null,
+    userInfo: {},
+    hasUserInfo: false,
+    nickName: '昵称',
+    showModal_logo: false,
+    storeType: '/img/storeType.png',
+    tenantName: '',
+    tanentType: '/img/tanentType.png',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function(options) {
+    var userInfo = wx.getStorageSync('userInfo');
+    if (userInfo.nickName == null || userInfo.nickName == '') {
+      this.setData({
+        showModal_logo: true,
+      });
+    }
+
+    this.myInformation()
+
+    let that = this;
+    var _userInfo = wx.getStorageSync('userInfo');
+    var memberCount = _userInfo.memberCount
+    var nickName = _userInfo.nickName
+    var myPhone = _userInfo.avatarUrl
+    that.setData({
+      memberCount: memberCount
+    });
+
+    if (nickName) {
+      that.setData({
+        nickName: nickName
+      });
+    }
+    if (myPhone) {
+      that.setData({
+        myPhone: myPhone
+      });
+    }
+
+    if (app.globalData.tenantName) {
+      this.setData({
+        tenantName: app.globalData.tenantName
+      })
+    }
+
+    if (app.globalData.tenantLength) {
+      this.setData({
+        tenantLength: app.globalData.tenantLength
+      })
+    }
+
+    if (app.globalData.tenantId) {
+      this.setData({
+        tenantId: app.globalData.tenantId
+      })
+    }
+
+
+    this.setData({
+      isShareholder: app.globalData.isShareholder
+    })
+
+    this.setData({
+      superStaff: app.globalData.superStaff
+    })
+
+
+    if (!app.globalData.tenantId) {
+      this.tenantList()
+    }
+
+  },
+  myInformation: function() {
+    var that = this
+    var data = {}
+    data.tenantId = app.globalData.tenantId
+    util.request(api.myInformation, data).then(function(res) {
+      if (res.errno == 0) {
+
+      }
+    });
+  },
+  tenantList: function() {
+    var that = this
+    var data = {};
+    util.request(api.tenantList, data).then(function(res) {
+      if (res.errno == 0) {
+        app.globalData.tenantLength = res.data.tenantList.length
+        app.globalData.tenantId = res.data.tenantList.slice(0, 1)[0]["tenantId"]
+        var datas = res.data.tenantList;
+        for (var i = 0; i < datas.length; i++) {
+          if (app.globalData.tenantId == res.data.tenantList[i]["tenantId"]) {
+            app.globalData.tenantName = res.data.tenantList[i]["tenantName"]
+          }
+        }
+        that.setData({
+          tenantName: app.globalData.tenantName,
+          tenantLength: app.globalData.tenantLength,
+        });
+        if(app.globalData.tenantId){
+          var that = this
+          var data = {};
+          data.tenantId = app.globalData.tenantId
+          util.request(api.checkShareHolder, data).then(function (res) {
+            if (res.errno == 0) {
+              if (res.data.state == true) {
+                var _userInfo = wx.getStorageSync('userInfo');
+                if (_userInfo) {
+                  _userInfo.memberCount = res.data.user.memberCount;
+                  wx.setStorageSync('userInfo', _userInfo);
+                }
+                if (res.data.user.isHolder) {
+                  app.globalData.isShareholder = res.data.user.isHolder
+                } else {
+                  app.globalData.isShareholder = 0
+                }
+                if (res.data.user.staffId) {
+                  app.globalData.superStaff = res.data.user.staffId
+                } else {
+                  app.globalData.superStaff = 0
+                }
+              }
+            }
+          });
+        }
+      }
+    });
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  // onShareAppMessage: function() {
+
+  // },
+
+  myEdit: function(e) {
+    var userInfo = wx.getStorageSync('userInfo');
+    if (userInfo.nickName == null || userInfo.nickName == '') {
+      this.setData({
+        showModal_logo: true,
+      });
+    } else {
+      var myPhone = this.data.myPhone
+      var pageUrl = encodeURIComponent('pages/my/edit/index?myPhone=' + myPhone)
+      var userInfo = wx.getStorageSync('userInfo');
+      if (userInfo.isRegister == 1) {
+        wx.navigateTo({
+          url: '../../../pages/my/edit/index',
+        })
+      } else {
+        wx.navigateTo({
+          url: '../../../pages/my/edit/login/index?pageUrl=' + pageUrl
+        })
+      }
+    }
+  },
+
+
+  myVip: function(e) {
+    var userInfo = wx.getStorageSync('userInfo');
+    if (userInfo.nickName == null || userInfo.nickName == '') {
+      this.setData({
+        showModal_logo: true,
+      });
+    } else {
+      var userInfo = wx.getStorageSync('userInfo');
+      if (userInfo) {
+        var memberCount = userInfo.memberCount
+      }
+      var isRegister = userInfo.isRegister
+      var myPhone = this.data.myPhone
+      var pageUrl = encodeURIComponent('pages/my/myVip/index?myPhone=' + myPhone)
+      if (isRegister > 0) {
+        if (memberCount < 1) {
+          wx.navigateTo({
+            url: '../../../pages/my/myVip/index'
+          })
+        } else {
+          wx.navigateTo({
+            url: '../../../pages/my/membership/card/index'
+          })
+        }
+      } else {
+        wx.navigateTo({
+          url: '../../../pages/my/edit/login/index?pageUrl=' + pageUrl
+        })
+      }
+    }
+  },
+  superStaff: function() {
+    var staffId = app.globalData.superStaff
+    wx.navigateTo({
+      url: '../../../pages/staff/login/index?isMy=1' + '&staffId=' + staffId,
+    })
+  },
+
+
+  introduce: function(e) {
+    wx.navigateTo({
+      url: '../../../pages/my/introduce/index/index',
+    })
+  },
+  order: function() {
+    wx.navigateTo({
+      url: '../../../pages/order/index/index?currentNavbar=0',
+    })
+  },
+  shareRed: function(e) {
+    wx.navigateTo({
+      url: '../../../pages/share/shareRed/index/index',
+    })
+  },
+  message: function(e) {
+    wx.navigateTo({
+      url: '../../../pages/action/message/index/index',
+    })
+  },
+  actionList: function(e) {
+    wx.navigateTo({
+      url: '../../../pages/action/list/tab/index',
+    })
+  },
+  note: function(e) {
+    wx.navigateTo({
+      url: '../../../pages/action/note/index/index',
+    })
+  },
+  storeType: function(e) {
+    wx.navigateTo({
+      url: '../../../pages/store/tenant/index',
+    })
+  }
+})
